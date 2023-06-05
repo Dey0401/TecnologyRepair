5257ba1dda19af588ffb8ea962555673ec639dec
Deyssi Vergara <deyssiyuliana@gmail.com>
2023-06-04 19:55:04 -0500

    Api (Get.Post,Put,Delete) To Repair Controller whithout database

---
+++ TecnologyRepair/TecnologyRepair.Backend/Controllers/ToRepairsController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TecnologyRepair.Shared.Entities;
+
+namespace TecnologyRepair.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToRepairsController : ControllerBase
+    {
+        private List<ToRepair> _toRepair;
+
+        public ToRepairsController()
+        {
+            _toRepair = new List<ToRepair>
+            {
+               new ToRepair {Id = 1, TypeOfEquipment = "", Brand = "", DateOfAdmission = DateTime.Now , RetirementDate = DateTime.Now, OwnerNames = "", OwnerLastName = "",  OwnerPhone = "", OwnerEmail = "", DiagnosisOfTheDamage = "", TechnicianComments = "" , RepairStatus = "", RepairPrice = 10000 },
+               new ToRepair {Id = 2, TypeOfEquipment = "", Brand = "", DateOfAdmission = DateTime.Now , RetirementDate = DateTime.Now, OwnerNames = "", OwnerLastName = "",  OwnerPhone = "", OwnerEmail = "", DiagnosisOfTheDamage = "", TechnicianComments = "" , RepairStatus = "", RepairPrice = 10000 },
+               new ToRepair {Id = 3, TypeOfEquipment = "", Brand = "", DateOfAdmission = DateTime.Now , RetirementDate = DateTime.Now, OwnerNames = "", OwnerLastName = "",  OwnerPhone = "", OwnerEmail = "", DiagnosisOfTheDamage = "", TechnicianComments = "" , RepairStatus = "", RepairPrice = 10000 },
+              };
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_toRepair);
+        }
+
+        [HttpPost]
+        public IActionResult Post(ToRepair toRepair)
+        {
+            _toRepair.Add(toRepair);
+
+            return Ok(toRepair);
+        }
+
+        [HttpPut]
+        public IActionResult Put(ToRepair toRepair)
+        {
+            var repair = _toRepair.FirstOrDefault(t => t.Id == toRepair.Id);
+            if (repair == null)
+            {
+                return NotFound();
+            }
+            repair.TypeOfEquipment = toRepair.TypeOfEquipment;
+            repair.Brand = toRepair.Brand;
+            repair.DateOfAdmission = DateTime.Now;
+            repair.RetirementDate = DateTime.Now;
+            repair.OwnerNames = toRepair.OwnerNames;
+            repair.OwnerLastName = toRepair.OwnerLastName;
+            repair.OwnerPhone = toRepair.OwnerPhone;
+            repair.OwnerEmail = toRepair.OwnerEmail;
+            repair.DiagnosisOfTheDamage = toRepair.DiagnosisOfTheDamage;
+            repair.TechnicianComments = toRepair.TechnicianComments;
+            repair.RepairStatus = toRepair.RepairStatus;
+            repair.RepairPrice = toRepair.RepairPrice;
+
+            return Ok(repair);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var repair = _toRepair.FirstOrDefault(t => t.Id == id);
+            if (repair == null)
+            {
+                return NotFound();
+            }
+
+            _toRepair.Remove(repair);
+            return NoContent();
+        }
+
+    }
+}
---
+++ TecnologyRepair/TecnologyRepair.Shared/Entities/ToRepair.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TecnologyRepair.Shared.Entities
+{
+    public class ToRepair
+    {
+        #region Properties
+        public int Id { get; set; }
+        public string TechnicianComments { get; set; } = null!;
+        [Required]
+        [MaxLength(200)]
+        public string TypeOfEquipment { get; set; } = null!;
+        public string Brand { get; set; } = null!;
+        public DateTime DateOfAdmission { get; set; }
+        public DateTime RetirementDate { get; set; }
+        public string OwnerNames { get; set; } = null!;
+        public string OwnerLastName { get; set; } = null!;
+        public string OwnerPhone { get; set; } = null!;
+        public string OwnerEmail { get; set; } = null!;
+        public string DiagnosisOfTheDamage { get; set; } = null!;
+        public string RepairStatus { get; set; } = null!;
+        public decimal RepairPrice { get; set; }
+
+        #endregion
+    }
+}
